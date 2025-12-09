5b2f59b27bfddd27de21fdb6df874b47383d690f
Asliddin0108 <156826188+Asliddin0108@users.noreply.github.com>
2025-12-09 14:40:35 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -373,7 +373,7 @@ def is_valid_order(text):
     return False
 
 # 🧭 Maqsad guruh
-DEST_CHAT_ID =  -5072775952
+DEST_CHAT_ID =  -1003454728629
 dest_entity = None
 
 # 📦 Takroriy xabarlar (1 daqiqa ichida)
